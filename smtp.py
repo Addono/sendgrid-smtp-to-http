4ed3d809c104d944071be780035b1fd6b74da0ef
Adriaan Knapen <a.d.knapen@protonmail.com>
2018-05-19 16:24:08 +0200

    fix: switched to using another part of the sendgrid api

--- smtp.py
+++ smtp.py
@@ -2,32 +2,77 @@ import re
 import asyncore
 from smtpd import SMTPServer
 import sendgrid
-import os
 from sendgrid.helpers.mail import *
+import os
+try:
+    # Python 3
+    import urllib.request as urllib
+except ImportError:
+    # Python 2
+    import urllib2 as urllib
 
 class SendGridHttp(SMTPServer):
+
+    def __init__(self, localaddr, remoteaddr):
+        SMTPServer.__init__(self, localaddr, remoteaddr)
+
+        with open('sendgrid_key', 'r+') as content_file:
+            data = content_file.read().strip()
+
+            if data is None or data == '':
+                print('No API key set in the file: sendgrid_key')
+                exit(1)
+
+            print('Sendgrid API key read from storage')
+            self.apikey = data
+
     def process_message(self, peer, mailfrom, rcpttos, data):
-        try:
-            sg = sendgrid.SendGridAPIClient(apikey=os.environ.get('SENDGRID_API_KEY'))
-            from_email = Email(self.find_header_value('From', data))
-            to_email = Email(self.find_header_value('To', data))
-            subject = self.find_header_value('Subject', data)
-            content = Content(self.find_header_value('Content-Type', data), re.match('\r?\n\r?\n([\s\S])*', data).group(1))
-            mail = Mail(from_email, subject, to_email, content)
-            response = sg.client.mail.send.post(request_body=mail.get())
-            print(response.status_code)
-            print(response.body)
-            print(response.headers)
-        except Exception as e:
-            print str(e)
+        for recipient in rcpttos:
+            try:
+                subject = self.find_header_value('Subject', data)
+                body = re.search(r'\n\n([\s\S]*)', data).group(1)
+                content_type = self.find_header_value('Content-Type', data)
+
+                sg = sendgrid.SendGridAPIClient(apikey=self.apikey)
+                data = {
+                    "personalizations": [
+                        {
+                            "to"           : [
+                                {
+                                    "email": recipient
+                                }
+                            ],
+                            "subject"      : subject
+                        },
+                    ],
+                    "from"            : {
+                        "email": mailfrom
+                    },
+                    "content"         : [
+                        {
+                            "type" : content_type,
+                            "value": body
+                        }
+                    ],
+                }
+                try:
+                    response = sg.client.mail.send.post(request_body=data)
+                except urllib.HTTPError as e:
+                    print (e.read())
+                    exit()
+                print(response.status_code)
+                print(response.body)
+                print(response.headers)
+            except Exception as e:
+                print str(e)
 
     def find_header_value(self, header, data):
-        regex = re.match('(\n|^)' + header + ': (\S)*', data)
+        regex = re.compile(header+': ([^\n]*)').search(data)
         if (regex is None):
             print('Could not find header: ' + header)
+            return
 
         return regex.group(1)
-
 
 
 def run():
