a4e0081daeaa2c677becc442e86dd1ea31a1ce54
Adriaan Knapen <a.d.knapen@protonmail.com>
2018-05-19 14:33:53 +0200

    fix: match did not have enough arguments

--- smtp.py
+++ smtp.py
@@ -9,10 +9,10 @@ class SendGridHttp(SMTPServer):
     def process_message(self, peer, mailfrom, rcpttos, data):
         try:
             sg = sendgrid.SendGridAPIClient(apikey=os.environ.get('SENDGRID_API_KEY'))
-            from_email = Email(self.find_header_value('From'))
-            to_email = Email(self.find_header_value('To'))
-            subject = self.find_header_value('Subject')
-            content = Content(self.find_header_value('Content-Type'), re.match('\n\n([\s\S])*').group(1))
+            from_email = Email(self.find_header_value('From', data))
+            to_email = Email(self.find_header_value('To', data))
+            subject = self.find_header_value('Subject', data)
+            content = Content(self.find_header_value('Content-Type', data), re.match('\n\n([\s\S])*', data).group(1))
             mail = Mail(from_email, subject, to_email, content)
             response = sg.client.mail.send.post(request_body=mail.get())
             print(response.status_code)
@@ -21,8 +21,8 @@ class SendGridHttp(SMTPServer):
         except Exception as e:
             print str(e)
 
-    def find_header_value(self, header):
-        regex = re.match('(\n|^)' + header + ': (\S)*')
+    def find_header_value(self, header, data):
+        regex = re.match('(\n|^)' + header + ': (\S)*', data)
         if (header is None):
             print('Could not find header: ' + header)
 
